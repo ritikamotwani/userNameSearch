e49f63bd91b13c0a3705671ab5cce63bd24391c7
ritikamotwani <ritika.motwani0712@gmail.com>
2019-03-04 15:31:46 +0800

    Run react create app

---
+++ search-app/src/actions.js
@@ -0,0 +1,18 @@
+export const t = {
+    LOAD_USER_DATA: 'LOAD_USER_DATA',
+    LOAD_USER_DATA_SUCCESS: 'LOAD_USER_DATA_SUCCESS'
+};
+
+export const actions = ({
+    loadUserData: name => ({
+        type: t.LOAD_USER_DATA,
+        name
+    }),
+
+    loadUserDataSuccess: data => ({
+        type: t.LOAD_USER_DATA_SUCCESS,
+        data
+    })
+});
+
+// actions that would be dispatched to the redux store
